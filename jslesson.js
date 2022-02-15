ef4de7e93866611af9317cacaa5f8331241193f4
Vitalii <mirakleee@gmail.com>
2022-02-15 22:45:48 +0200

    Задание1

--- jslesson.js
+++ jslesson.js
@@ -1,2 +1,17 @@
-"usestrict";
+const numberOfFilms = +prompt("Скольк фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies:{},
+  actor: {},
+  genres:[],
+  privat: false
+};
+
+
+
+const a = prompt('Один из послюпрос',''),
+b = prompt('ocenka',''),
+c = prompt('Один из послюпрос',''),
+d = prompt('Один из послюпрос','');
 
