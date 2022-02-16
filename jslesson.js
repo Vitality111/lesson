3a99e356dcff8705c32820275a52f4f1567c95b2
Vitalii <mirakleee@gmail.com>
2022-02-16 22:15:36 +0200

    Второй урок, цыклы

--- jslesson.js
+++ jslesson.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const numberOfFilms = +prompt("Скольк фильмов вы уже посмотрели?", "");
 
 const personalMovieDB = {
@@ -8,13 +10,61 @@ const personalMovieDB = {
   privat: false,
 };
 
-const a = prompt("Один из послюпрос", ""),
-  b = prompt("ocenka", ""),
-  c = prompt("Один из послюпрос", ""),
-  d = prompt("ocenka", "");
 
+// Автоматизация с помошью цикла
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// первый способ цыкла!!!!!!!!!!!!!!!!!
+for (let i = 0; i < 1; i++) {
+  const a = prompt("Один из понравившихся", ""),
+    b = prompt("ocenka", "");
+
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log("done");
+  } else {
+    console.log("error!");
+    i--;
+  }
+}
+// Второй способ цыкла!!!!!!!!!!!!!!!!!!!!!!!!1
+// let i = 0;
+// while (i < 1) {
+//   const a = prompt("Один из понравившихся", ""),
+//   b = prompt("ocenka", "");
+
+//   if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log("done");
+//         break;
+//       } else {
+//         console.log("error!");
+//         i--;
+//       }
+//     }
+
+//// Третий способ!!!!!!!!!!!!!!!!1
+// let i = 0;
+// do {
+//   const a = prompt("Один из понравившихся", ""),
+//     b = prompt("ocenka", "");
+//   if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//     personalMovieDB.movies[a] = b;
+//     console.log("done");
+//     break;
+//   } else {
+//     console.log("error!");
+//     i--;
+//   }
+// } while (i < 1);
+
+// if (personalMovieDB.count < 10) {
+//   console.log("Malo");
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//   console.log("norm");
+// } else if (personalMovieDB.count >= 30) {
+//   console.log("nu=ihuia sebe");
+// } else {
+//   console.log("eroree");
+// }
 
 console.log(personalMovieDB);
